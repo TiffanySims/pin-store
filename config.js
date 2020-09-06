db3da354ccad3cf2999b2623c5541b1490d649d8
Tiffany Sims <Tiffany@news-MBP.attlocal.net>
2020-09-05 22:39:24 -0500

    CSS changes

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://tiffany:Fashion1@cluster0-hnozq.mongodb.net/test",
-  secret: "fdofjourn"
-};
---
+++ configSample.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "your mongoURI",
+  secret: "your secret key",
+};
